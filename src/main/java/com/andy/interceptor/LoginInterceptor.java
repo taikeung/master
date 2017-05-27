5d3ab0a63cadfc49160795207e99256eca4cccf6
AndyHooo <396877565@qq.com>
2017-05-27 11:24:29 +0800

    /

---
+++ src/main/java/com/andy/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.andy.interceptor;
+
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 添加登录拦截器
+ */
+public class LoginInterceptor implements HandlerInterceptor{
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
