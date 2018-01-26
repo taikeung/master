043ead63608750ab016383614456efc19af61af9
hudaqiang <hudaqiang@172.25.69.54>
2018-01-26 14:32:47 +0800

    添加注释

--- src/main/java/com/andy/quartz/AutoWiringSpringBeanJobFactory.java
+++ src/main/java/com/andy/quartz/AutoWiringSpringBeanJobFactory.java
@@ -5,25 +5,23 @@ import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
-
 /**
- * @author Freud
+ * @ClassName AutoWiringSpringBeanJobFactory
+ * @Description 自动注入
+ * @author hudaqiang
+ * @Date Jan 26, 2018 2:31:45 PM
  */
-public class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware
-{
-    private transient AutowireCapableBeanFactory beanFactory;
-    
-    public void setApplicationContext(final ApplicationContext context)
-    {
-        beanFactory = context.getAutowireCapableBeanFactory();
-    }
-    
-    @Override
-    protected Object createJobInstance(final TriggerFiredBundle bundle)
-        throws Exception
-    {
-        final Object job = super.createJobInstance(bundle);
-        beanFactory.autowireBean(job);
-        return job;
-    }
+public class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+	private transient AutowireCapableBeanFactory beanFactory;
+
+	public void setApplicationContext(final ApplicationContext context) {
+		beanFactory = context.getAutowireCapableBeanFactory();
+	}
+
+	@Override
+	protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+		final Object job = super.createJobInstance(bundle);
+		beanFactory.autowireBean(job);
+		return job;
+	}
 }
